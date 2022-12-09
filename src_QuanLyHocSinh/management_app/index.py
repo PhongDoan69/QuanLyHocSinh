332c06d4fbcb1febf325518c95676544947098bd
PhongDoan69 <phongdoantrung1@gmail.com>
2022-12-09 17:55:20 +0700

    commit path param và query params

--- src_QuanLyHocSinh/management_app/index.py
+++ src_QuanLyHocSinh/management_app/index.py
@@ -1,7 +1,8 @@
 from management_app import app
-from flask import render_template
+from flask import render_template, request
 import utils
 import json
+
 
 @app.route("/")
 def home():
@@ -9,11 +10,13 @@ def home():
     return render_template('index.html', grades=grades)
 
 
-
 @app.route("/students")
 def student_list():
-    students = utils.load_students()
+    g_id = request.args.get("grade_id")
+    students = utils.load_students(g_id=g_id)
+
     return render_template('students.html', students=students)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src_QuanLyHocSinh/management_app/utils.py
+++ src_QuanLyHocSinh/management_app/utils.py
@@ -1,12 +1,23 @@
 import json, os
 from management_app import app
 
+
 def read_json(path):
     with open(path, "r") as f:
         return json.load(f)
 
+
 def load_grades():
     return read_json(os.path.join(app.root_path, 'data/grades.json'))
 
-def load_students():
-    return read_json(os.path.join(app.root_path, 'data/students.json'))
+
+def load_students(g_id=None, kw=None, to_total_score=None):
+    students = read_json(os.path.join(app.root_path, 'data/students.json'))
+    if g_id:
+        students = [g for g in students if g['grade_id'] == int(g_id)]
+    if kw:
+        students = [g for g in students if g['name'].lower().find(kw.lower) >= 0]
+    if to_total_score:
+        students = [g for g in students if g['total_score'] == float(to_total_score)]
+
+    return students
